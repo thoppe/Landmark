119209cf36732a751e88e90d0ea037ab03778f8e
Travis Hoppe <travis.hoppe@gmail.com>
2017-09-25 00:38:00 -0400

    Updates fabfile for seed access

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,9 @@
 from fabric.api import local
 
+def dev():
+    # Creates a dev network with a known seed
+    local("testrpc --seed 2048")
+
 def serve():
     local("cp -rf build docs/")
     local("npm run dev")
